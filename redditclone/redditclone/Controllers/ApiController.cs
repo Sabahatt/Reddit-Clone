754a953e687d27beb564f56415420b37f5d55234
Thanwar <60439635+Thanwar@users.noreply.github.com>
2022-08-01 20:02:27 +0500

    added api controller

---
+++ redditclone/redditclone/Controllers/ApiController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace redditclone.Controllers
+{
+    public class ApiController : Controller
+    {
+        // GET: Api
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Signup()
+        {
+            //return Json("Signup");
+            string output = "Signup";
+            return Json(output, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Login()
+        {
+            string output = "Login";
+            return Json(output, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
